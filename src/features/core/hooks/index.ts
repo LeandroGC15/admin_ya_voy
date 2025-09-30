44c6a8c0a496a6e0858f9d34847893f431ea2fcd
Leandro <leacamacho2002@gmail.com>
2025-09-30 11:16:18 -0400

    Borrando archivos duplicados

--- src/features/core/hooks/index.ts
+++ src/features/core/hooks/index.ts
@@ -1,2 +1,1 @@
-export { useCrudData } from './use-crud-data';
 
--- src/features/users/hooks/index.ts
+++ src/features/users/hooks/index.ts
@@ -1,4 +1,2 @@
 export * from './use-users';
 export * from './use-user-stats';
-export * from './use-user-search';
-export * from './use-user-operations';
